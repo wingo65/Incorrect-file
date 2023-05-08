bb3a81aa4bdd05dd3f7cc05d44c17a3964a16599
wingo65 <nelkhaouli@email.neit.edu>
2023-05-08 11:15:38 -0400

    done

---
+++ w6_spring_2022/script.js
@@ -0,0 +1,26 @@
+var tray = document.querySelector(`#tray`);
+
+tray.addEventListener(`click`, openClose)
+
+function openClose(e)
+{
+    var nav = document.querySelector(`nav`);
+    nav.classList.toggle(`hidden`);
+}
+
+var tabs = document.querySelectorAll(`#tabs a`);
+console.log(tabs);
+
+for(let i=0; i<tabs.length; i++)
+{
+    tabs[i].addEventListener(`click`, function(e){
+        for(let i=0; i<tabs.length; i++)
+        {
+            tabs[i].style.backgroundColor=`#373737`;
+        } 
+        e.target.style.backgroundColor=`#161616`;
+        document.querySelectorAll(`#breadcrumbs a`)[3].innerHTML = `Tabs ${i+1}`
+    });
+}
+
+
